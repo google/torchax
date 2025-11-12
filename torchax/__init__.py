eea53a4e7872132c50f74c1b7eb5fca0e2e19c61
Han Qi <qihan.dev@gmail.com>
2025-11-12 22:41:14 +0000

    Fix static docs page build workflow
    * So the install command need to run without working dir
    
    Also bumps version in __init__.py; as by convention the
    in-development is one version newer than the last released
    version

--- torchax/__init__.py
+++ torchax/__init__.py
@@ -22,7 +22,7 @@ from torch.utils import _pytree as pytree
 from torchax import tensor
 from contextlib import contextmanager
 
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 VERSION = __version__
 
 # the "fast path" uses some sparse tensor thingies that currently we
