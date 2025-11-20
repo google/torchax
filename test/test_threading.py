59595b67d8951746488d53b3b08a6b83ac2cc504
Jialei Chen <jialeic@google.com>
2025-11-20 09:41:35 -0800

    remove white space

--- test/test_threading.py
+++ test/test_threading.py
@@ -21,17 +21,17 @@ class TestThreading(unittest.TestCase):
 
   def test_access_config_thread(reraise):
     torchax.default_env()
-  
+
     def task():
       with reraise:
         print(torchax.default_env().param)
-  
+
     threads = []
     for _ in range(5):
       thread = threading.Thread(target=task, args=())
       thread.start()
       threads.append(thread)
-  
+
     for thread in threads:
       thread.join()
 
